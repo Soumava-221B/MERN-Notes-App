e3711dc3b1b845bb61d3f5d7d8b20fa484eb5dc1
Soumava Das <soumavadas06@gmail.com>
2025-06-06 11:13:09 +0530

    create page completed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import api from "../lib/axios";
 import toast from "react-hot-toast";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
@@ -13,13 +13,13 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
         console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
       } catch (error) {
         console.log("Error fetching notes");
-        console.log(error)
+        console.log(error);
         if (error.response?.status === 429) {
           setIsRateLimited(true);
         } else {
@@ -35,12 +35,14 @@ const HomePage = () => {
 
   return (
     <div className="min-h-screen">
-      <Navbar /> 
+      <Navbar />
 
       {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
